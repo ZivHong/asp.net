701ca36b768db2702a05c2fc246ffa11624029ea
hong <rock33332@gmail.com>
2020-04-14 23:14:24 +0800

    alter database(alter member primary key(id),del phone). login page

---
+++ login/App_Code/Class1.cs
@@ -0,0 +1,40 @@
+﻿using System.Data.OleDb;
+
+namespace mydb //定義一個namespace
+{
+    public class DB  //創造一個叫DB的物件
+
+    { //封裝欄位，protected 表示可適用於該物件及其繼承的物件
+        protected OleDbDataReader dbread = null;
+        protected OleDbCommand cmd = new OleDbCommand();
+        protected OleDbConnection conn = new OleDbConnection("Provider= Microsoft.Jet.OLEDB.4.0; Data Source = |DataDirectory|/dvd.mdb");
+    }
+    public class Nonquery : DB  //繼承DB
+    {
+        public void Execute(string sql)  //public 是大家都可用
+        {
+            conn.Open();
+            cmd = new OleDbCommand(sql, conn);
+            cmd.ExecuteNonQuery();    //執行nonquery程式，沒有從資料庫取資料
+            conn.Close();
+        }
+    }
+    public class Query : DB   //繼承DB
+    {
+        public string Execute(string sql)
+        {
+            string result = "";
+            cmd = new OleDbCommand(sql, conn);
+            conn.Open();
+            dbread = cmd.ExecuteReader();   //執行reader，讀取資料庫回傳資料
+            dbread.Read();//開始讀取
+            for (int i = 0; i < 4; i++)    //把讀來的結果放進result
+            {
+                result += dbread.GetString(i) + ",";//用逗號分隔欄位，方便後台處理字串
+            }
+            dbread.Close();
+            conn.Close();
+            return result;  //回傳result給呼叫此物件的程式
+        }
+    }
+}
---
+++ login/loginpage.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Data.OleDb;
+using System.Web.Security;
+
+public partial class _Default : System.Web.UI.Page
+{
+    OleDbDataReader dbread = null;
+    OleDbCommand cmd = new OleDbCommand();
+    OleDbConnection conn = new OleDbConnection("Provider= Microsoft.Jet.OLEDB.4.0; Data Source = |DataDirectory|/dvd.mdb");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void ValidateUser(object sender, System.Web.UI.WebControls.AuthenticateEventArgs e)
+    {
+        string userName = login.UserName;
+        string password = login.Password;
+        bool result = dblogin(userName, password);
+        if ((result))
+        {
+            e.Authenticated = true;
+            Response.Redirect("normal.html");
+        }
+        else
+        {
+            e.Authenticated = false;
+        }
+       
+    }
+    protected bool dblogin(string userName, string password) 
+    {
+        cmd.CommandType = CommandType.Text;
+        cmd.CommandText = "select * from member where username = :UserName and password = :Password";
+        cmd.Parameters.AddWithValue("UserName", userName);
+        cmd.Parameters.AddWithValue("Password", password);
+        cmd.Connection = conn;
+        conn.Open();
+        string result = Convert.ToString(cmd.ExecuteScalar());
+        if (string.IsNullOrEmpty(result))
+        {
+            //invalid user/password , return flase
+            return false;
+        }
+        else
+        {
+            // valid login
+            return true;
+        }
+    }
+}
